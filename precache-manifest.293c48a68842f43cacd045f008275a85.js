4f016d0aa2ab632843c30ac12765dc1f482ec893
신혜 김 <eheex01@gmail.com>
2019-08-06 14:35:16 +0900

    Updates

---
+++ precache-manifest.293c48a68842f43cacd045f008275a85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aff527c3f1356322bc899a4919fca8af",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "10c354271f4de04871b4",
+    "url": "/my-app/static/css/main.ac081d05.chunk.css"
+  },
+  {
+    "revision": "e083292f352b8a48b197",
+    "url": "/my-app/static/js/2.61ebbd48.chunk.js"
+  },
+  {
+    "revision": "10c354271f4de04871b4",
+    "url": "/my-app/static/js/main.82c89b38.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
